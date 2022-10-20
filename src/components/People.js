68e5fd0d5d53296b3b49b4eef10064c146a4918a
tribagus <tri.bagus21@gmail.com>
2022-10-20 19:46:28 +0700

    fetch data for people comp and create new person comp

--- src/components/People.js
+++ src/components/People.js
@@ -1,7 +1,27 @@
+import { useQuery } from "@tanstack/react-query"
+import Person from "./Person"
+
+const fetchPeople = async () => {
+  const res = await fetch("http://swapi.dev/api/people/")
+  return res.json()
+}
+
 const People = () => {
+  const { data, status } = useQuery(["people"], fetchPeople)
+  console.log(data)
+
   return (
     <div className="people">
       <h2>People</h2>
+
+      {status === "loading" && <div>Loading data...</div>}
+
+      {status === "error" && <div>Error fetching data</div>}
+
+      {status === "success" &&
+        data.results.map((person) => (
+          <Person key={person.name} person={person} />
+        ))}
     </div>
   )
 }
