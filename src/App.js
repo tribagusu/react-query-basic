bf90eb3bcd20048c60695e45ffd6171bdc33a842
tribagus <tri.bagus21@gmail.com>
2022-10-20 18:53:19 +0700

    fetch data from api and create planet comp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { useState } from "react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
 import Navbar from "./components/Navbar"
 import People from "./components/People"
 import Planets from "./components/Planets"
+
+const queryClient = new QueryClient()
 
 function App() {
   const [page, setPage] = useState("planets")
 
   return (
-    <div className="App">
-      <h1>Star Wars Info</h1>
-      <Navbar setPage={setPage} />
-      <div className="content">
-        {page === "planets" ? <Planets /> : <People />}
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Star Wars Info</h1>
+        <Navbar setPage={setPage} />
+        <div className="content">
+          {page === "planets" ? <Planets /> : <People />}
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   )
 }
 
